4eb8b645416a57b1a925a179ffc1ae244ec938e9
dakochan666 <adrianmuhammadkuncoro@gmail.com>
2020-10-29 00:39:55 +0700

    fix-error

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,8 @@ const LandingController = require('../controllers/LandingController');
 const PLayListController = require('../controllers/PlaylistController'); 
 const router = require('express').Router();
 
-const authentication = require('../middlewares/authentication')
-const authorization = require('../middlewares/authorization')
+const authentication = require('../middleware/authentication')
+const authorization = require('../middleware/authorization')
 
 //Router Landing ================================================================
 router.post('/register', LandingController.register)
@@ -12,12 +12,12 @@ router.post('/login', LandingController.login)
 router.use(authentication)
 
 //Router Playlist ================================================================
-router.get('/playlist', PLayListController.readPlaylist)
-router.post('/playlist', PLayListController.createPlaylist)
-router.get('/playlist/:id', PLayListController.getPlaylistById)
-router.put('/playlist/:id', authorization, PLayListController.updateAllPlaylistId)
-router.patch('/playlist/:id', authorization, PLayListController.updateSongPlaylistId)
-router.delete('/playlis/:id', authorization, PLayListController.deletePlaylistId)
+// router.get('/playlist', PLayListController.readPlaylist)
+// router.post('/playlist', PLayListController.createPlaylist)
+// router.get('/playlist/:id', PLayListController.getPlaylistById)
+// router.put('/playlist/:id', authorization, PLayListController.updateAllPlaylistId)
+// router.patch('/playlist/:id', authorization, PLayListController.updateSongPlaylistId)
+// router.delete('/playlis/:id', authorization, PLayListController.deletePlaylistId)
 
 
 module.exports = router;
