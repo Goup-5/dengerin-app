c12ddb2ea0a32ae2160975451ee85305d9b3fdf3
RamaRamesses <maulanaribrahim@outlook.com>
2020-10-28 23:17:56 +0700

    Updated migrations & models property

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,11 @@
 const PLayListController = require('../controllers/PlaylistController'); 
+const UserController = require('../controllers/UserController'); 
 const router = require('express').Router();
 
 router.get('/', PLayListController.index)
+
+//User
+router.post('/register', UserController.register)
+
 
 module.exports = router;
