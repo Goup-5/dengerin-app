eaf3deb21038cfc17616bcb168dcec261e305bf1
RamaRamesses <maulanaribrahim@outlook.com>
2020-10-29 12:15:13 +0700

    minor changes

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,26 +3,26 @@ const PLayListController = require('../controllers/PlaylistController');
 const UserController = require('../controllers/UserController'); 
 const router = require('express').Router();
 
-const authentication = require('../middlewares/authentication')
-const authorization = require('../middlewares/authorization')
+const authentication = require('../middleware/authentication')
+const authorization = require('../middleware/authorization')
 
 //Router Landing ================================================================
-router.post('/register', LandingController.register)
-router.post('/login', LandingController.login)
+// router.post('/register', UserController.register)
+// router.post('/login', LandingController.login)
 
-router.use(authentication)
+// router.use(authentication)
 
-//Router Playlist ================================================================
-router.get('/playlist', PLayListController.readPlaylist)
-router.post('/playlist', PLayListController.createPlaylist)
-router.get('/playlist/:id', PLayListController.getPlaylistById)
-router.put('/playlist/:id', authorization, PLayListController.updateAllPlaylistId)
-router.patch('/playlist/:id', authorization, PLayListController.updateSongPlaylistId)
-router.delete('/playlis/:id', authorization, PLayListController.deletePlaylistId)
+// //Router Playlist ================================================================
+// router.get('/playlist', PLayListController.readPlaylist)
+// router.post('/playlist', PLayListController.createPlaylist)
+// router.get('/playlist/:id', PLayListController.getPlaylistById)
+// router.put('/playlist/:id', authorization, PLayListController.updateAllPlaylistId)
+// router.patch('/playlist/:id', authorization, PLayListController.updateSongPlaylistId)
+// router.delete('/playlis/:id', authorization, PLayListController.deletePlaylistId)
 
 
-//User
-router.post('/register', UserController.register)
+// User
+// router.post('/register', UserController.register)
 
 
 module.exports = router;
