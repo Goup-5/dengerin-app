29748047554eaa6ebf40cb95a7653b2e25ec3c9b
dakochan666 <adrianmuhammadkuncoro@gmail.com>
2020-10-29 09:38:56 +0700

    1. add 3rd API deezer 2. fixing authentication 3. fixing login and register

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,3 +1,4 @@
+const APIController = require('../controllers/APIController');
 const LandingController = require('../controllers/LandingController');
 const PLayListController = require('../controllers/PlaylistController'); 
 const router = require('express').Router();
@@ -18,6 +19,10 @@ router.use(authentication)
 // router.put('/playlist/:id', authorization, PLayListController.updateAllPlaylistId)
 // router.patch('/playlist/:id', authorization, PLayListController.updateSongPlaylistId)
 // router.delete('/playlis/:id', authorization, PLayListController.deletePlaylistId)
+
+//Router 3rd Party API !!!! ini masih gua taro sebelum authen, nanti mah taro dibawah, soalnya authen-nya belum bener
+router.post('/search', APIController.searchDeezer)
+
 
 
 module.exports = router;
